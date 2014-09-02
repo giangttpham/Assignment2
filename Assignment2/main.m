ff9c8fb7e67717b40d9997e3f97bc7f32db9e9e8
Giang Pham <giangttpham@gmail.com>
2014-09-01 19:14:30 -0700

    Initial Commit

---
+++ Assignment2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Assignment2
+//
+//  Created by Tra` Beo' on 9/1/14.
+//  Copyright (c) 2014 Giang Pham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
